ebd91fc7d23f2055d651180b2f82329cf8d8c33f
Alpet Gexha <agexha@gmail.com>
2021-03-28 14:11:25 +0200

    Gur leter gersher Game

--- lojëra.php
+++ lojëra.php
@@ -29,7 +29,7 @@ include('items/need_to_login.php');
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
 
-	<?php include('items/navbar.php'); ?>
+	<?php include 'items/navbar.php'; ?>
 
 	<div class="container mt-5">
 </head>
@@ -51,7 +51,7 @@ include('items/need_to_login.php');
       <h1>Emri i lojes</h1> 
       <p></p> 
       <p>pershkrimi</p>
-      <a href="#"><button class="btn btn-danger">Luaj Tani</button></a>
+      <a href="loja/gur-leter-gersher.php"><button class="btn btn-danger">Luaj Tani</button></a>
     </div>
   </div>
 </div>
