689d4762a4b291452482e58231997fa0ab750c0d
Alpet Gexha <agexha@gmail.com>
2021-05-01 21:00:47 +0200

    add pagination on admin-post

--- admin/post_admin.php
+++ admin/post_admin.php
@@ -1,97 +1,109 @@
  <?php
-  $msg = "";
-  session_start();
-  include '../config.php';
-  include '../items/need_to_login.php';
+    $msg = "";
+    session_start();
+    include '../config.php';
+    include '../items/need_to_login.php';
 
-  //Nese e ke rolin Admin(1) shko në admin control panel
-  if (isset($_SESSION['ROLE']) &&  $_SESSION['ROLE'] != '1') {
-    header('location: ../index.php');
-    die();
-  }
-  ?>
+    //Nese e ke rolin Admin(1) shko në admin control panel
+    if (isset($_SESSION['ROLE']) &&  $_SESSION['ROLE'] != '1') {
+        header('location: ../index.php');
+        die();
+    }
+    ?>
 
  <!DOCTYPE html>
  <html>
 
  <head>
-   <?php include '../items/title_bar_img.php'; ?>
+     <?php include '../items/title_bar_img.php'; ?>
 
-   <title>Postimet e Userit</title>
+     <title>Postimet e Userit</title>
 
-   <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-   <link rel="stylesheet" type="text/css" href="../assets/css/loading.css">
+     <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+     <link rel="stylesheet" type="text/css" href="../assets/css/loading.css">
 
-   <!-- ------------ Foto per title bar ------------------ -->
-   <?php include('../items/title_bar_img.php'); ?>
+     <!-- ------------ Foto per title bar ------------------ -->
+     <?php include('../items/title_bar_img.php'); ?>
 
-   <!-- ------------ Meta ------------------ -->
-   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-   <meta charset="utf-8">
-   <!-- ------------ Boostrap css ------------------ -->
-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+     <!-- ------------ Meta ------------------ -->
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+     <meta charset="utf-8">
+     <!-- ------------ Boostrap css ------------------ -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
  </head>
 
  <body>
 
-   <!-- ------------ Boostrap JS ------------------ -->
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+     <!-- ------------ Boostrap JS ------------------ -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
 
-   <!-- ------------ Style ------------------ -->
-   <style type="text/css">
-     .jumbotron {
-       position: relative;
-       border: 2px black solid;
-       background-color: #e9ecef;
-       padding: 40px;
-     }
+     <!-- ------------ Style ------------------ -->
+     <style type="text/css">
+         .jumbotron {
+             position: relative;
+             border: 2px black solid;
+             background-color: #e9ecef;
+             padding: 40px;
+         }
 
-     .delete {
-       position: absolute;
-       top: 25px;
-       right: 20px;
-     }
-   </style>
-   <?php include '../items/admin_navbar.php'; ?>
-   <div class="container mt-5">
+         .delete {
+             position: absolute;
+             top: 25px;
+             right: 20px;
+         }
+     </style>
+     <?php include '../items/admin_navbar.php'; ?>
+     <div class="container mt-5">
+         <form class="form-inline" method="get" action="#" style="margin:10px;">
+             <input class="form-control mr-sm-2" type="search" placeholder="Search" name="search">
+             <button id="btn_search" class="btn btn-outline-primary" type="submit">Search</button>
+         </form>
 
-     <form class="form-inline" method="get" action="#" style="margin:10px;">
-       <input class="form-control mr-sm-2" type="search" placeholder="Search" name="search">
-       <button id="btn_search" class="btn btn-outline-primary" type="submit">Search</button>
-     </form>
-     <?php
+         <?php
+            if (isset($_GET['page_no']) && $_GET['page_no'] != "") {
+                $page_no = $_GET['page_no'];
+            } else {
+                $page_no = 1;
+            }
 
-      //shfaqj e postimeve
-      $select = "SELECT u.emri, u.mbiemri, p.emri_post, u.username, p.body, p.image, p.post_time,  p.id from post p, users u where p.user_id = u.id  order by p.id DESC ";
-      $result   = mysqli_query($db, $select);
+            $total_records_per_page = 10;
+            $offset = ($page_no - 1) * $total_records_per_page;
+            $previous_page = $page_no - 1;
+            $next_page = $page_no + 1;
+            $adjacents = "2";
 
-      //shfaqja e te gjithave postimeve 
-      while (($row = $result->fetch_assoc()) != null) {
+            $result_count = mysqli_query($db, "SELECT COUNT(*) As total_records FROM post ");
+            $total_records = mysqli_fetch_array($result_count);
+            $total_records = $total_records['total_records'];
+            $total_no_of_pages = ceil($total_records / $total_records_per_page);
+            $sedbd_last = $total_no_of_pages - 1; // total page minus 1
 
-        echo "<div class='jumbotron'>";
-        echo "<h1 class='display-4'>" . $row['emri_post'] . "</h1>";
-        echo "<div class='post_foto'>";
-        echo "<a target='_blank' href=../'assets/post_images/" . $row['image'] . "'>";
-        echo "<img src='../assets/post_images/" . $row['image'] . "' >";
-        echo "</a>";
-        echo "</div>";
-        echo "<div class='delete'>";
-        echo "
+            $result = mysqli_query($db, "SELECT u.emri, u.mbiemri, p.emri_post, u.username, p.body, p.image, p.post_time,  p.id from post p, users u where p.user_id = u.id  order by p.id DESC  LIMIT $offset, $total_records_per_page");
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<div class='jumbotron'>";
+                echo "<h1 class='display-4'>" . $row['emri_post'] . "</h1>";
+                echo "<div class='post_foto'>";
+                echo "<a target='_blank' href=../'assets/post_images/" . $row['image'] . "'>";
+                echo "<img src='../assets/post_images/" . $row['image'] . "' >";
+                echo "</a>";
+                echo "</div>";
+                echo "<div class='delete'>";
+                echo "
         <td>
          <button type='button' class='btn btn-danger' data-toggle='modal' data-target='#post2_" . $row['id'] . "'>
        Fshije
       </button> </td> ";
-        echo "</div>";
-        echo " <p class='lead'>" . $row['body'] . "</p>";
-        echo "<hr class='my-4'>";
-        echo "<p>Postuesi: " . $row['emri'] . " " . $row['mbiemri'] . "</p>";
-        echo "<div class= 'post_time'";
-        echo " <p class='lead'>" . $row['post_time'] . "</p>";
-        echo "</div>";
-        echo "</div>";
-        echo '
+                echo "</div>";
+                echo " <p class='lead'>" . $row['body'] . "</p>";
+                echo "<hr class='my-4'>";
+                echo "<p>Postuesi: " . $row['emri'] . " " . $row['mbiemri'] . "</p>";
+                echo "<div class= 'post_time'";
+                echo " <p class='lead'>" . $row['post_time'] . "</p>";
+                echo "</div>";
+                echo "</div>";
+                echo '
               <div class="modal fade" id="post2_' . $row['id'] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                   <div class="modal-content">
@@ -112,17 +124,107 @@
                 </div>
               </div>
               ';
-      }
-      //nese nuk ka postime
-      if (mysqli_num_rows($result) == 0) {
-        echo "<p style='text-align:center; color:red; font-size:20px'> Nuk ka rezultate";
-      }
-      ?>
+            }
+            mysqli_close($db);
+            ?>
 
+         <?php
+
+
+            //nese nuk ka postime
+            if (mysqli_num_rows($result) == 0) {
+                echo "<p style='text-align:center; color:red; font-size:20px'> Nuk ka rezultate";
+            }
+            ?>
+
+
+
+
+
+         <div class="page-count" style='padding: 10px 20px 0px; border-top: dotted 1px #CCC;'>
+             <strong>Page <?php echo $page_no . " of " . $total_no_of_pages; ?></strong>
+         </div>
+
+         <ul class="pagination">
+             <?php // if($page_no > 1){ echo "<li><a href='?page_no=1'>First Page</a></li>"; } 
+                ?>
+
+             <li <?php if ($page_no <= 1) {
+                        echo "class='disabled'";
+                    } ?>>
+                 <a <?php if ($page_no > 1) {
+                        echo "href='?page_no=$previous_page'";
+                    } ?>>Prapa</a>
+             </li>
+
+             <?php
+                if ($total_no_of_pages <= 10) {
+                    for ($counter = 1; $counter <= $total_no_of_pages; $counter++) {
+                        if ($counter == $page_no) {
+                            echo "<li class='active'><a>$counter</a></li>";
+                        } else {
+                            echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                        }
+                    }
+                } elseif ($total_no_of_pages > 10) {
+
+                    if ($page_no <= 4) {
+                        for ($counter = 1; $counter < 8; $counter++) {
+                            if ($counter == $page_no) {
+                                echo "<li class='active'><a>$counter</a></li>";
+                            } else {
+                                echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                            }
+                        }
+                        echo "<li><a>...</a></li>";
+                        echo "<li><a href='?page_no=$sedbd_last'>$sedbd_last</a></li>";
+                        echo "<li><a href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+                    } elseif ($page_no > 4 && $page_no < $total_no_of_pages - 4) {
+                        echo "<li><a href='?page_no=1'>1</a></li>";
+                        echo "<li><a href='?page_no=2'>2</a></li>";
+                        echo "<li><a>...</a></li>";
+                        for ($counter = $page_no - $adjacents; $counter <= $page_no + $adjacents; $counter++) {
+                            if ($counter == $page_no) {
+                                echo "<li class='active'><a>$counter</a></li>";
+                            } else {
+                                echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                            }
+                        }
+                        echo "<li><a>...</a></li>";
+                        echo "<li><a href='?page_no=$sedbd_last'>$sedbd_last</a></li>";
+                        echo "<li><a href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+                    } else {
+                        echo "<li><a href='?page_no=1'>1</a></li>";
+                        echo "<li><a href='?page_no=2'>2</a></li>";
+                        echo "<li><a>...</a></li>";
+
+                        for ($counter = $total_no_of_pages - 6; $counter <= $total_no_of_pages; $counter++) {
+                            if ($counter == $page_no) {
+                                echo "<li class='active'><a>$counter</a></li>";
+                            } else {
+                                echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                            }
+                        }
+                    }
+                }
+                ?>
+
+             <li <?php if ($page_no >= $total_no_of_pages) {
+                        echo "class='disabled'";
+                    } ?>>
+                 <a <?php if ($page_no < $total_no_of_pages) {
+                        echo "href='?page_no=$next_page'";
+                    } ?>>Tjetra</a>
+             </li>
+             <?php if ($page_no < $total_no_of_pages) {
+                    echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+                } ?>
+         </ul>
+     </div>
      <!-- Loading  -->
      <div class="loader loader-default" data-text="Duke u Fshir"></div>
      <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
      <script src="assets/js/function.js"> </script>
  </body>
 
- </html>
+ </html>    
